15d6b361c0090871eb8c4dc175973d49aa72c005
Thomas Lee <thomas.j.lee@gmail.com>
2010-04-05 13:23:18 -0400

    adding iphone path

--- urls.py
+++ urls.py
@@ -5,9 +5,9 @@ from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    
-    (r'^/admin/', include(admin.site.urls)),
+urlpatterns = patterns('',    
+    (r'^/admin/', include(admin.site.urls)),)
     (r'^/', include('gate.urls')),
+    (r'^iphone/', direct_to_template, {'template': 'iphone.html'})
     (r'^.*', direct_to_template, {'template': 'default.html'})
 )
