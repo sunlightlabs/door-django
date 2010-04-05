c5ab0123897acf7cd734803e74da1aa2bad85e3c
Thomas Lee <thomas.j.lee@gmail.com>
2010-04-05 17:19:39 -0400

    iphone HTML5 application

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # Django settings for gatekeeper project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -35,17 +36,9 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+MEDIA_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'media')
+MEDIA_URL = '/media/'
+ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = ''
@@ -63,13 +56,13 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
-ROOT_URLCONF = 'gatekeeper.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/templates/')
+    '/home/keymaster/gatekeeper/templates/',)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -79,3 +72,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'gate'
 )
+
+try:
+    from local_settings import *
+except Exception, e:
+    pass
--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+import settings
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
@@ -5,9 +6,18 @@ from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',    
-    (r'^/admin/', include(admin.site.urls)),)
-    (r'^/', include('gate.urls')),
-    (r'^iphone/', direct_to_template, {'template': 'iphone.html'})
-    (r'^.*', direct_to_template, {'template': 'default.html'})
+urlpatterns = patterns('',
+    # (r'^/admin/', include(admin.site.urls)),
+    # (r'^/', include('gate.urls')),
+    (r'^iphone/', direct_to_template, {'template': 'iphone.html'}),
+    # (r'^.*', direct_to_template, {'template': 'default.html'}),
 )
+
+if settings.DEBUG:
+    from django.views.static import serve
+    _media_url = settings.MEDIA_URL
+    if _media_url.startswith('/'):
+        _media_url = _media_url[1:]
+        urlpatterns += patterns('',
+            (r'^%s(?P<path>.*)$' % _media_url, serve, {'document_root': settings.MEDIA_ROOT}))
+    del(_media_url, serve)
